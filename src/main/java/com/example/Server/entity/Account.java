e3e8f954c7c0aa516ad4bc61787035ac497d01f4
Cullen Vaughan <cullenvaughan@proton.me>
2024-12-11 13:28:44 -0600

    Added Entity classes

---
+++ src/main/java/com/example/Server/entity/Account.java
@@ -0,0 +1,98 @@
+package com.example.Server.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Column(name = "accountId")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer accountId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    public Account() {}
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.role = "Employee";
+    }
+
+    public Account(Integer accountId, String username, String password, String role) {
+        this.accountId = accountId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Account other = (Account) obj;
+        if (accountId == null) {
+			if (other.accountId != null) return false;
+		} else if (!accountId.equals(other.accountId)) return false;
+        if (password == null) {
+			if (other.password != null) return false;
+		} else if (!password.equals(other.password)) return false;
+        if (username == null) {
+			if (other.username != null) return false;
+		} else if (!username.equals(other.username)) return false;
+        if (role == null) {
+			if (other.role != null) return false;
+		} else if (!role.equals(other.role)) return false;
+		return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Account {" + 
+                " accountId = " + accountId +
+                ", username = '" + username + '\'' +
+                ", password = '" + password + '\'' +
+                ", role = '" + role + '\'' + '}';
+    }
+}
---
+++ src/main/java/com/example/Server/entity/Ticket.java
@@ -0,0 +1,132 @@
+package com.example.Server.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ticket")
+public class Ticket {
+    @Column(name = "ticketId")
+    @Id
+    @GeneratedValue
+    private Integer ticketId;
+
+    @Column(name = "ticketSubmittedBy")
+    private String ticketSubmittedBy;
+
+    @Column(name = "ticketType")
+    private String ticketType;
+
+    @Column(name = "ticketAmount")
+    private Integer ticketAmount;
+
+    @Column(name = "ticketDescription")
+    private String ticketDescription;
+
+    @Column(name = "ticketStatus")
+    private String ticketStatus;
+
+    public Ticket() {}
+
+    public Ticket(String ticketSubmittedBy, String ticketType, Integer ticketAmount, String ticketDescription) {
+        this.ticketSubmittedBy = ticketSubmittedBy;
+        this.ticketType = ticketType;
+        this.ticketAmount = ticketAmount;
+        this.ticketDescription = ticketDescription;
+        this.ticketStatus = "Pending";
+    }
+
+    public Ticket(Integer ticketId, String ticketSubmittedBy, String ticketType, Integer ticketAmount, String ticketDescription, String ticketStatus) {
+        this.ticketId = ticketId;
+        this.ticketSubmittedBy = ticketSubmittedBy;
+        this.ticketType = ticketType;
+        this.ticketAmount = ticketAmount;
+        this.ticketDescription = ticketDescription;
+        this.ticketStatus = ticketStatus;
+    }
+
+    public Integer getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Integer ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTicketSubmittedBy() {
+        return ticketSubmittedBy;
+    }
+
+    public void setTicketSubmittedBy(String ticketSubmittedBy) {
+        this.ticketSubmittedBy = ticketSubmittedBy;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public Integer getTicketAmount() {
+        return ticketAmount;
+    }
+
+    public void setTicketAmount(Integer ticketAmount) {
+        this.ticketAmount = ticketAmount;
+    }
+
+    public String getTicketDescription() {
+        return ticketDescription;
+    }
+
+    public void setTicketDescription(String ticketDescription) {
+        this.ticketDescription = ticketDescription;
+    }
+
+    public String getTicketStatus() {
+        return ticketStatus;
+    }
+
+    public void setTicketStatus(String ticketStatus) {
+        this.ticketStatus = ticketStatus;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Ticket other = (Ticket) obj;
+        if (ticketId == null) {
+			if (other.ticketId != null) return false;
+		} else if (!ticketId.equals(other.ticketId)) return false;
+        if (ticketSubmittedBy == null) {
+			if (other.ticketSubmittedBy != null) return false;
+		} else if (!ticketSubmittedBy.equals(other.ticketSubmittedBy)) return false;
+        if (ticketType == null) {
+			if (other.ticketType != null) return false;
+		} else if (!ticketType.equals(other.ticketType)) return false;
+        if (ticketAmount == null) {
+			if (other.ticketAmount != null) return false;
+		} else if (!ticketAmount.equals(other.ticketAmount)) return false;
+        if (ticketDescription == null) {
+			if (other.ticketDescription != null) return false;
+		} else if (!ticketDescription.equals(other.ticketDescription)) return false;
+        if (ticketStatus == null) {
+			if (other.ticketStatus != null) return false;
+		} else if (!ticketStatus.equals(other.ticketStatus)) return false;
+		return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket {" + 
+                " ticketId = " + ticketId +
+                ", ticketSubmittedBy = '" + ticketSubmittedBy + '\'' +
+                ", ticketType = '" + ticketType + '\'' +
+                ", ticketAmount = '" + ticketAmount + '\'' +
+                ", ticketDescription = '" + ticketDescription + '\'' +
+                ", ticketStatus = '" + ticketStatus + '\'' + '}';
+    }
+}
